f92d8228ea925bf5fcb4cc4c49945318960f9498
Mastersam07 <abadasamuelosp@gmail.com>
2022-06-27 13:13:26 +0100

    :sparkles: feat: auth - login

---
+++ lib/features/login/presentation/views/login.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:smarty/core/navigation/navigator.dart';
+import 'package:smarty/res/res.dart';
+
+import '../../../../widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 109),
+            Image.asset("assets/images/logo.png",
+                color: SmartyColors.primary, width: 174),
+            const SizedBox(height: 64),
+            Text(
+              'Login to your account',
+              style: TextStyles.headline4.copyWith(
+                  color: SmartyColors.primary, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 48),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Email'),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your email',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Password'),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your password',
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 64),
+            AppButtonPrimary(
+              label: 'Login',
+              onPressed: () =>
+                  AppNavigator.pushNamedReplacement(dashboardRoute),
+            ),
+            const Spacer(),
+            GestureDetector(
+              onTap: () => AppNavigator.pushNamed(registerRoute),
+              child: Text(
+                'Don\'t have an account? Create account',
+                style: TextStyles.body,
+              ),
+            ),
+            const SizedBox(height: 58),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:smarty/res/res.dart';
+
+enum BtnState { active, inactive }
+
+class AppButtonPrimary extends StatelessWidget {
+  const AppButtonPrimary({
+    Key? key,
+    this.onPressed,
+    required this.label,
+    this.btnColor,
+  }) : super(key: key);
+
+  final VoidCallback? onPressed;
+  final String label;
+  final Color? btnColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final effectiveBackgroundColor = btnColor ?? SmartyColors.primary;
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(
+          label,
+          style: TextStyles.body.copyWith(color: SmartyColors.tertiary),
+        ),
+        style: TextButton.styleFrom(
+          primary: effectiveBackgroundColor,
+          backgroundColor: effectiveBackgroundColor,
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            side: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
